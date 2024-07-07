3e7b445f76636e736f1e0e2e03675ffa2a3572a7
Adrian Joost <adi.joost@gmail.com>
2024-07-07 21:34:41 +0200

    added sysargs

--- calculateLove.py
+++ calculateLove.py
@@ -5,52 +5,32 @@ import sys
 from tqdm import tqdm
 from utils.plotter3d import plot3d, contPlot3d
 
-NUMBER_OF_YEARS_LIVED = 10
-NUMBER_OF_LOVERS = 200
-DISTRIBUTION_MEAN = 0.5
-DISTRIBUTION_SIGMA = 0.3
+NUMBER_OF_SIGMAS = 20
+NUMBER_OF_MEANS = 20
+TARGET_YEAR = 10
+NUMBER_OF_YEARS_LIVED=12
+NUMBER_OF_LOVERS=20
 
 def main():
-    sigmas = np.linspace(0,1,20)
-    means = np.linspace(0,1,20)
+    sigmas = np.linspace(0,1,NUMBER_OF_SIGMAS)
+    means = np.linspace(0,1,NUMBER_OF_MEANS)
     results = []
     resultMeans = []
     resultSigmas = []
     for sigma in sigmas:
         for mean in means:
-            result = runTargetSimulation(mean, sigma, numbersOfLovers=20)
+            result = runTargetSimulation(mean, sigma, targetYear=TARGET_YEAR, numbersOfYears=NUMBER_OF_YEARS_LIVED, numbersOfLovers=NUMBER_OF_LOVERS)
             results.append(result)
             resultMeans.append(mean)
             resultSigmas.append(sigma)
     saveResults(means, sigmas, results)
-    contPlot3d(means, sigmas, results)
+    #contPlot3d(means, sigmas, results)
 
 def saveResults(means, sigmas, results):
     resultsAsArray = np.asarray(results)
     np.save("sigmas.npy", sigmas)
     np.save("means.npy", means)
     np.save("results.npy", resultsAsArray)
-    
-
-def runNormalSimulation():
-    lovers = []
-    timeEvolution = []
-    notLovedChances = []
-    x = np.random.normal(DISTRIBUTION_MEAN, DISTRIBUTION_SIGMA, NUMBER_OF_LOVERS)
-    for chance in x:
-        lovers.append(Lover(chance, 0.5))
-    for _ in tqdm(range(NUMBER_OF_YEARS_LIVED)):
-        countNotLovers = 0
-        for lover in tqdm(lovers):
-            lover.liveYear()
-            if (lover.hasNeverLoved()):
-                countNotLovers += 1
-        timeEvolution.append(countNotLovers)
-#    for lover in lovers:    
-#        if (lover.hasNeverLoved()):
-#            notLovedChances.append(lover.getLoverChance())
-    relativeChange = getRelativeChange(timeEvolution)
-    saveResultAsJSON(notLovedChances, timeEvolution, relativeChange)
 
 def runTargetSimulation(mean, sigma, targetYear=10, numbersOfYears=12, numbersOfLovers=10000):
     lovers = []
@@ -72,20 +52,6 @@ def runTargetSimulation(mean, sigma, targetYear=10, numbersOfYears=12, numbersOf
     relativeChange = getRelativeChange(timeEvolution)
     return relativeChange[targetYear]
 
-
-def saveResultAsJSON(notLovedChances, timeEvolution, relativeChange: np.ndarray, name="results.json"):
-    saveParams = {
-        "numberOfLovers": NUMBER_OF_LOVERS,
-        "numberOfYearsLived:": NUMBER_OF_YEARS_LIVED,
-        "distributionMean": DISTRIBUTION_MEAN,
-        "distributionSigma": DISTRIBUTION_SIGMA,
-        "notLovedChances": notLovedChances,
-        "timeEvolution": timeEvolution,
-        "relativeChange": relativeChange.tolist(),
-        }
-    with open(name, "w", encoding="utf-8") as file:
-        file.write(json.dumps(saveParams))
-
 def getRelativeChange(timeEvolution) -> list:
     returnList = np.zeros(len(timeEvolution))
     returnList[0] = (timeEvolution[0] / float(NUMBER_OF_LOVERS))
@@ -99,10 +65,11 @@ def getRelativeChange(timeEvolution) -> list:
     return returnList    
 
 if __name__ == "__main__":
-    if len(sys.argv) == 5:
-        NUMBER_OF_YEARS_LIVED = int(sys.argv[1])
-        NUMBER_OF_LOVERS = int(sys.argv[2])
-        DISTRIBUTION_MEAN = float(sys.argv[3])
-        DISTRIBUTION_SIGMA = float(sys.argv[4])
+    if len(sys.argv) == 6:
+        NUMBER_OF_SIGMAS = int(sys.argv[1])
+        NUMBER_OF_MEANS = int(sys.argv[2])
+        TARGET_YEAR = int(sys.argv[3])
+        NUMBER_OF_YEARS_LIVED= int(sys.argv[4])
+        NUMBER_OF_LOVERS = int(sys.argv[5])
     main()
 
